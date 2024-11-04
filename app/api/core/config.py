e082ba34894016e11701f225ccbe0c9909fe6999
slytvynenko <sergey159123@gmail.com>
2024-11-04 18:08:40 +0200

    Changing the DB to RDS

--- app/api/core/config.py
+++ app/api/core/config.py
@@ -1,6 +1,8 @@
 import os
 
-SQL_SERVER = os.getenv('SQL_SERVER', 'localhost')
-SQL_DATABASE = os.getenv('SQL_DATABASE', 'notes_api')
+SQL_SERVER = os.getenv('SQL_SERVER')
+SQL_DATABASE = os.getenv('SQL_DATABASE')
+SQL_USERNAME = os.getenv('SQL_USERNAME')
+SQL_PASSWORD = os.getenv('SQL_PASSWORD')
 
-DATABASE_URI = f"mssql+pyodbc://{SQL_SERVER}/{SQL_DATABASE}?driver=ODBC+Driver+17+for+SQL+Server"
+DATABASE_URI = f"mssql+pyodbc://{SQL_USERNAME}:{SQL_PASSWORD}@{SQL_SERVER}/{SQL_DATABASE}?driver=ODBC+Driver+17+for+SQL+Server"
