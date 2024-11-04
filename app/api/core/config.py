bd78545061bae035e08f39aad9e5396dd10cf3c6
slytvynenko <sergey159123@gmail.com>
2024-11-04 23:26:02 +0200

    Commit cache

--- app/api/core/config.py
+++ app/api/core/config.py
@@ -4,3 +4,4 @@ SQL_SERVER = os.getenv('SQL_SERVER', 'localhost')
 SQL_DATABASE = os.getenv('SQL_DATABASE', 'notes_api')
 
 DATABASE_URI = f"mssql+pyodbc://{SQL_SERVER}/{SQL_DATABASE}?driver=ODBC+Driver+17+for+SQL+Server"
+git
